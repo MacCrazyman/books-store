907b6822cd81276797fb27dc0e1ad2ad0f2a95cd
Leonardo Pareja Pareja <leonardopareja.fl@hotmail.com>
2022-01-17 16:31:31 -0500

    Remove boilerplate files

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import {
   BrowserRouter, Route, Routes,
 } from 'react-router-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import Books from './components/books';
 import Categories from './components/categories';
 
@@ -22,8 +21,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root'),
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
