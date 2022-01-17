444f2adf39759c7ecf75f11eee385e12a5b438f7
Leonardo Pareja Pareja <leonardopareja.fl@hotmail.com>
2022-01-17 17:13:41 -0500

    Include Book component

--- src/components/books.js
+++ src/components/books.js
@@ -9,7 +9,12 @@ const Books = () => (
           ADD NEW BOOK
           <input placeholder="Book title" type="text" required name="bookTitle" id="bookTitle" />
         </label>
-        <input placeholder="Category" required />
+        <input list="categories" placeholder="Category" required />
+        <datalist id="categories">
+          <option value="cat1">cat1</option>
+          <option value="cat2">cat2</option>
+          <option value="cat3">cat3</option>
+        </datalist>
         <button type="submit">ADD BOOK</button>
       </form>
     </div>
