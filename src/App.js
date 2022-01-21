37c870bb16d7927e9574cdda2368f30951c50d26
Leonardo Pareja Pareja <leonardopareja.fl@hotmail.com>
2022-01-21 12:14:35 -0500

    Add styling to header

--- src/App.js
+++ src/App.js
@@ -2,20 +2,27 @@ import './App.css';
 import {
   Link, Outlet,
 } from 'react-router-dom';
+import loginIcon from './img/loginIcon.svg';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <nav>
+        <nav className="App-nav">
           <h1>Bookstore CMS</h1>
           <Link to="/">Books</Link>
           <Link to="/categories">Categories</Link>
+          <div className="login-icon">
+            <img src={loginIcon} alt="login icon" />
+          </div>
         </nav>
       </header>
       <main>
         <Outlet />
       </main>
+      <footer>
+        <a href="http://www.onlinewebfonts.com">oNline Web Fonts</a>
+      </footer>
     </div>
   );
 }
